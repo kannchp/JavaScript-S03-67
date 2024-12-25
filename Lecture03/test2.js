f6c0994d4691cf2af6d2c8f4bddb4207184c3092
kannchp <139094458+kannchp@users.noreply.github.com>
2024-12-25 12:54:31 +0700

    test2.js

--- Lecture03/test2.js
+++ Lecture03/test2.js
@@ -6,7 +6,7 @@ let funiture = ['Table','chair','Couch'];
 for (let index = 0; index < funiture.length; index++) {
     const name = funiture[index];
     console.log(name);
-    //for(let i = 0; i < 3 ; i++){
+    //for(let i = 0; i < name.length ; i++){
     //const namechar = name[i];
     //console.log(namechar);
     //}
