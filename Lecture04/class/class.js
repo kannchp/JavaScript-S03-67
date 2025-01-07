e38055b28a1208d945932e02d6c102868575a81e
Nichapa <s6606021630029@email.kmutnb.ac.th>
2025-01-07 15:34:48 +0700

    Lecture-04-ClassCompatModule

---
+++ Lecture04/class/class.js
@@ -0,0 +1,13 @@
+class Song{
+    constructor() {
+        this.title;
+        this.author;
+    }
+    
+    play() {
+        console.log("Song planying!");
+    }
+}
+
+const mySong = new Song();
+mySong.play();
