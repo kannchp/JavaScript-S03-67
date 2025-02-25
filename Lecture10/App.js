ed60aa8e0f3f07ab00f814a55fcd850c13f2c3ee
kannchp <139094458+kannchp@users.noreply.github.com>
2025-02-25 23:13:58 +0700

    Add files via upload

---
+++ Lecture10/App.js
@@ -0,0 +1,39 @@
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>
+            {product.name}
+        </span>;
+}
+
+/*อันนี้ไม่แน่ใจดูหน้างานอีกทีเหมือนโค้ดไม่ครบ*/
+
+function ProductTable({ products }) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category} />
+            );
+        }
+        rows.push(
+            <ProductRow
+            product={product}
+            key={product.name} />
+        );
+        lastCategory = product.category;
+    });
+}
